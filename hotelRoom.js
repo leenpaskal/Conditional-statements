f059ad7dda61cc67a17e3da4725577e9afabdcfc
Paskal Leen <leenpaskal@gmail.com>
2025-07-16 21:28:02 +0300

    Update hotelRoom.js

--- hotelRoom.js
+++ hotelRoom.js
@@ -5,22 +5,22 @@ let priceApartment = 0;
 
 if(mesec == "May" || mesec == "October"){
 
-    priceApartment=65;
+    priceApartment = 65;
   
-    if(broy>7 && broy<=14){
-        priceStudio=50*0.95;
-    }else if(broy>14){
-        priceStudio=50*0.7;
+    if(broy > 7 && broy <= 14){
+        priceStudio = 50 * 0.95;
+    }else if(broy > 14){
+        priceStudio = 50 * 0.7;
     }else{
     priceStudio = 50;
     }
 
 }else if(mesec == "June" || mesec == "September"){
 
-    priceApartment=68.70;
+    priceApartment= 68.70;
     
-    if(broy>14){
-        priceStudio=75.20*0.8;
+    if(broy > 14){
+        priceStudio = 75.20 * 0.8;
     }else{
     priceStudio = 75.20;
     }
@@ -30,12 +30,12 @@ if(mesec == "May" || mesec == "October"){
     priceApartment = 77;
 }
 
-if(broy>14){
+if(broy > 14){
     priceApartment *= 0.9;
 }
 
-console.log(`Apartment: ${(priceApartment*broy).toFixed(2)} lv.`)
-console.log(`Studio: ${(priceStudio*broy).toFixed(2)} lv.`)
+console.log(`Apartment: ${(priceApartment * broy).toFixed(2)} lv.`)
+console.log(`Studio: ${(priceStudio * broy).toFixed(2)} lv.`)
 
 
 
