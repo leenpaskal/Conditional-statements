c0c2afb966acba5b350de00696d4b4f177ab82a6
Paskal Leen <leenpaskal@gmail.com>
2025-07-16 21:08:05 +0300

    Create hotelRoom.js
    Хотелска стая
    
    Хотел предлага 2 вида стаи: студио и апартамент. Напишете функция, която изчислява цената за целия престой за студио и апартамент. Цените зависят от месеца на престоя:
    
    Май и Oктомври 
    Юни и Септември 
    Юли и Август
    
    Студио – 50 лв./нощувка
    Студио – 75.20 лв./нощувка 
    Студио – 76 лв./нощувка
    
    Апартамент – 65 лв./нощувка
    Апартамент – 68.70 лв./нощувка
    Апартамент – 77 лв./нощувка
    
    Предлагат се и следните отстъпки:
    
    · За студио, при повече от 7 нощувки през май и октомври : 5% намаление.
    
    · За студио, при повече от 14 нощувки през май и октомври : 30% намаление.
    
    · За студио, при повече от 14 нощувки през юни и септември: 20% намаление.
    
    · За апартамент, при повече от 14 нощувки, без значение от месеца : 10% намаление.
    
    Вход
    
    Получават се 2 аргумента:
    
    · месецът – May, June, July, August, September или October
    
    · броят на нощувките – цяло число в интервала [0 … 200]
    
    Изход
    
    Да се отпечатат на конзолата 2 реда:
    
    · На първия ред: “Apartment: {цена за целият престой} lv.”
    
    · На втория ред: “Studio: {цена за целият престой} lv.”
    
    Цената за целия престой форматирана с точност до два знака след десетичната запетая.

---
+++ hotelRoom.js
@@ -0,0 +1,44 @@
+function hotelRoom(mesec, broy){
+
+let priceStudio = 0;
+let priceApartment = 0;
+
+if(mesec == "May" || mesec == "October"){
+
+    priceApartment=65;
+  
+    if(broy>7 && broy<=14){
+        priceStudio=50*0.95;
+    }else if(broy>14){
+        priceStudio=50*0.7;
+    }else{
+    priceStudio = 50;
+    }
+
+}else if(mesec == "June" || mesec == "September"){
+
+    priceApartment=68.70;
+    
+    if(broy>14){
+        priceStudio=75.20*0.8;
+    }else{
+    priceStudio = 75.20;
+    }
+
+}else if(mesec == "July" || mesec == "August"){
+    priceStudio = 76;
+    priceApartment = 77;
+}
+
+if(broy>14){
+    priceApartment *= 0.9;
+}
+
+console.log(`Apartment: ${(priceApartment*broy).toFixed(2)} lv.`)
+console.log(`Studio: ${(priceStudio*broy).toFixed(2)} lv.`)
+
+
+
+}
+
+hotelRoom("May", 15);
